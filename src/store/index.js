75f3bebe2bdbab57d88ff9bcf2940636ea3d32cb
lizezhong <l1103884823@outlook.com>
2019-07-25 20:18:43 +0800

    增加js-cookie,管理后台发来的token,增加请求头Authorization

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {login} from '@/api/auth'
+import { login } from '@/api/auth'
+import { getToken, setToken } from '../util/auth';
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        
+        token: getToken(),
     },
     mutations: {
+        SET_TOKEN: (state, token) => {
+            state.token = token
+        },
     },
     actions: {
         Login({ commit }, form) {
@@ -17,10 +21,15 @@ const store = new Vuex.Store({
             const code = form.code.trim();
             // action异步处理，dispatch可以知道action什么时候执行结束
             return new Promise((resolve, reject) => {
-                login(username, password, code).then(res => {
-                    // if (res.data.code == 10000) {
-                    //     console.log(res.data.msg);
-                    // }
+                login(username, password, code).then(response => {
+                    if (response.code == 10000) {
+                        const data = response.data
+                        console.log(data)
+                        const tokenStr = data.token
+                        setToken(tokenStr)
+                        console.log("getToken = " + getToken())
+                        commit('SET_TOKEN', tokenStr)
+                    }
                     resolve()
                 }).catch(error => {
                     reject(error);
--- src/util/request.js
+++ src/util/request.js
@@ -1,9 +1,67 @@
 import Axios from "axios";
+import store from '@/store/index'
+import { getToken } from '@/util/auth'
+import { Message, MessageBox } from 'element-ui'
 
-
- const request= Axios.create({
+const request = Axios.create({
     baseURL: "http://localhost",
     timeout: 15000,
-    headers: {'authorization':"1234567890"},
-}) ;
+    headers: { 'Authorization': "" },
+});
+
+// request拦截器
+request.interceptors.request.use(config => {
+    if (store.state.token) {
+        config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    }
+    return config
+}, error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
+})
+
+// respone拦截器
+request.interceptors.response.use(
+    response => {
+    /**
+    * code为非200是抛错 可结合自己业务进行修改
+    */
+      const res = response.data
+      if (res.code !== 10000) {
+          console.log(res.msg)
+        Message({
+          message: res.msg,
+          type: 'error',
+          duration: 3 * 1000
+        })
+  
+        // 401:未登录;
+        // if (res.code === 401||res.code === 403) {
+        //   MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
+        //     confirmButtonText: '重新登录',
+        //     cancelButtonText: '取消',
+        //     type: 'warning'
+        //   }).then(() => {
+        //     store.dispatch('FedLogOut').then(() => {
+        //       location.reload()// 为了重新实例化vue-router对象 避免bug
+        //     })
+        //   })
+        // }
+        return Promise.reject('error')
+      } else {
+        return response.data
+      }
+    },
+    error => {
+      console.log('err' + error)// for debug
+      Message({
+        message: error.message,
+        type: 'error',
+        duration: 3 * 1000
+      })
+      return Promise.reject(error)
+    }
+  )
+
 export default request
